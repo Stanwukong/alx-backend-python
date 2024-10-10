8f6520af99fd056da69afc745a22ff30a56619a2
Stanwukong <stanleyajanaku@gmail.com>
2024-10-10 13:26:37 +0000

    Basic annotations: floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Calculates the floor of a float."""
+import math
+
+
+def floor(n: float) -> int:
+    """
+       Finds the floor of n.
+    """
+    return math.floor(n)
